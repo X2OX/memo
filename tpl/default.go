053fcbcbb2729d9f5efa6c1a4056e90fc5d90be9
Aoang <aoang@x2ox.com>
2021-07-05 17:32:23 +0800

    Update html template

--- tpl/default.go
+++ tpl/default.go
@@ -10,14 +10,14 @@ const defaultHTML = `
 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1,maximum-scale=5,viewport-fit=cover">
 <title>Notes</title>
 <meta name="robots" content="noindex, nofollow">
-<link rel="apple-touch-icon" sizes="180x180" href="https://cdn.jsdelivr.net/gh/x2ox/note@f5ea0450547408cf661331f18b6da04120bae500/.data/static/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="https://cdn.jsdelivr.net/gh/x2ox/note@f5ea0450547408cf661331f18b6da04120bae500/.data/static/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="https://cdn.jsdelivr.net/gh/x2ox/note@f5ea0450547408cf661331f18b6da04120bae500/.data/static/favicon-16x16.png">
-<link rel="manifest" href="https://cdn.jsdelivr.net/gh/x2ox/note@master/.data/static/site.webmanifest">
-<link rel="mask-icon" href="https://cdn.jsdelivr.net/gh/x2ox/note@f5ea0450547408cf661331f18b6da04120bae500/.data/static/safari-pinned-tab.svg" color="#ffffff">
+<link rel="apple-touch-icon" sizes="180x180" href="https://cdn.jsdelivr.net/gh/x2ox/memo@f535d41e57af7da22a96d3fbab8bd8f3bddab3c1/.data/static/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://cdn.jsdelivr.net/gh/x2ox/memo@f535d41e57af7da22a96d3fbab8bd8f3bddab3c1/.data/static/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://cdn.jsdelivr.net/gh/x2ox/memo@f535d41e57af7da22a96d3fbab8bd8f3bddab3c1/.data/static/favicon-16x16.png">
+<link rel="manifest" href="https://cdn.jsdelivr.net/gh/x2ox/memo@f535d41e57af7da22a96d3fbab8bd8f3bddab3c1/.data/static/site.webmanifest">
+<link rel="mask-icon" href="https://cdn.jsdelivr.net/gh/x2ox/memo@f535d41e57af7da22a96d3fbab8bd8f3bddab3c1/.data/static/safari-pinned-tab.svg" color="#ffffff">
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="theme-color" content="#ffffff">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/x2ox/note@f5ea0450547408cf661331f18b6da04120bae500/.data/static/markdown.css"/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/x2ox/memo@d6e74cc79f80ce7da9977df834f593346806f964/.data/static/markdown.css"/>
 <style>
 #preview-box {
 	background-color: rgba(1,1,1,0.1);
